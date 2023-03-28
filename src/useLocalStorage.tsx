8cfb2b7d8a53c8f08ff6941c19e5241dd0567db0
Jeremyjdz13 <46872022+Jeremyjdz13@users.noreply.github.com>
2023-03-28 16:11:43 -0700

    Added test

--- src/useLocalStorage.tsx
+++ src/useLocalStorage.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react'
-// Passing a generic type T so we type useLocalStorage as generic type T and we are passing a key type string and initial value type generic value type T or a function (useState) () => T
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-    //Does the value exits yet?
     const [value, setValue ] = useState<T>(() => {
         const jsonValue = localStorage.getItem(key)
         if (jsonValue === null) {
@@ -14,7 +12,6 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
             return JSON.parse(jsonValue)
         }
     })
-    //Update when the value changes
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value))
     }, [value, key])
